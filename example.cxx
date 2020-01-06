0d6eabde6a8002d06e0de0ac237b953ae92ab796
jairoegc <jairoegc@gmail.com>
2020-01-06 12:13:20 -0300

    Minor fixes

--- example.cxx
+++ example.cxx
@@ -43,15 +43,14 @@ int main()
   act.sa_flags = 0;
   
 
-  printf("#############################################################\n
-          #                                                           #\n
-          #         C A E N    D I G I T I Z E R    S I L A B         #\n
-          #                          UTFSM                            #\n
-          #                                                           #\n
-          #############################################################\n
-          \n
-          Exit: Ctrl+c\n
-          Force Exit: Ctrl+\\\n\n");
+  printf("#############################################################\n"
+         "#                                                           #\n"
+         "#         C A E N    D I G I T I Z E R    S I L A B         #\n"
+         "#                          UTFSM                            #\n"
+         "#                                                           #\n"
+         "#############################################################\n\n"
+         "Exit: Ctrl+c\n"
+         "Force Exit: Ctrl+\\\n\n");
 
   //benchmark mide el desempeño del programa, no es vital
   bench = new TBenchmark();
@@ -67,14 +66,14 @@ int main()
 
   dig->setModel(model);
   dig->setTriggerPolarity(triggerpolaritymode);
-  printf("Digitizer initialization...\n\n
-          ----------------------------------------------------------\n")
+  printf("Digitizer initialization...\n\n"
+          "----------------------------------------------------------\n")
   if (dig->init()>=0) {
     dig->getInfo();
     printf("----------------------------------------------------------\n\n")
 
-    printf("Digitizer Configuration...\n\n
-          ----------------------------------------------------------\n")
+    printf("Digitizer Configuration...\n\n"
+          "----------------------------------------------------------\n")
     dig->setPolarizationType(polarization);//rango de polarizacion
     printf("polarization: %d\n",polarization);
     
@@ -86,8 +85,8 @@ int main()
     printf("----------------------------------------------------------\n\n")
 
     //Medición
-    printf("Starting Data Adquisition...\n\n
-          ----------------------------------------------------------\n")
+    printf("Starting Data Adquisition...\n\n"
+          "----------------------------------------------------------\n")
     dig->newFile("data_from_digitizer.root");
     sigaction(SIGINT, &act, 0); 
     for (int k=0;k<NBunch;k++)
